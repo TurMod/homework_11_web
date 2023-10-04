89aed69533fed0dda072ed034008339e15f1756a
TurMod <turbo.moderator@gmail.com>
2023-10-03 23:42:23 -0400

    homework_13_web

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 from fastapi import FastAPI, HTTPException, Depends, status
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi_limiter import FastAPILimiter
 
 from db.connect_db import get_db
 from sqlalchemy import text
 from sqlalchemy.orm import Session
+from conf.config import settings
 
-from routes import contacts, auth
+from routes import contacts, auth, users
+import redis.asyncio as redis
 
 app = FastAPI()
 
+origins = ['localhost:3000']
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
 app.include_router(contacts.router, prefix='/api')
 app.include_router(auth.router, prefix='/api')
+app.include_router(users.router, prefix='/api')
 
 @app.get("/api/healthchecker")
 def healthchecker(db: Session = Depends(get_db)):
@@ -24,3 +38,8 @@ def healthchecker(db: Session = Depends(get_db)):
         print(e)
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                             detail="Error connecting to the database")
+
+@app.on_event('startup')
+async def startup():
+    r = redis.Redis(host=settings.redis_host, port=settings.redis_port, db=0, encoding='utf-8', decode_responses=True)
+    await FastAPILimiter.init(r)
